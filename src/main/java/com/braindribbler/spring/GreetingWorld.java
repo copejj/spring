aa01d46b438bf19d682180547d7049687a33c741
Jeff Cope <jeff.cope@gmail.com>
2026-01-08 12:44:23 -0700

    Change to greeting

--- src/main/java/com/braindribbler/spring/GreetingWorld.java
+++ src/main/java/com/braindribbler/spring/GreetingWorld.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorld {
+public class GreetingWorld {
 
 	@GetMapping("/hello")
 	public String sayHello(@RequestParam(value = "name", defaultValue = "Hunnie") String name) {
@@ -15,5 +15,10 @@ public class HelloWorld {
 	@GetMapping
 	public String greet() {
 		return "Welcome to the Spring Boot Application!";
+	}
+
+	@GetMapping("/goodbye")
+	public String sayGoodbye(@RequestParam(value = "name", defaultValue = "Zippo") String name) {
+		return String.format("Goodbye, %s!", name);
 	}
 }
