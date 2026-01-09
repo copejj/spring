783c6b3950f44eec94830767d4f9f31381f6483b
Jeff Cope <jeff.cope@gmail.com>
2026-01-09 12:17:52 -0700

    changes to build to a war file

--- src/main/java/com/braindribbler/spring/Application.java
+++ src/main/java/com/braindribbler/spring/Application.java
@@ -2,11 +2,12 @@ package com.braindribbler.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
