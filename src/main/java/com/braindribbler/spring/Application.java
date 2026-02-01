85e6a15fb7dd59258913a759f6d308283c2e3ffb
Jeff Cope <jeff.cope@gmail.com>
2026-01-31 17:35:18 -0700

    testing github build

--- src/main/java/com/braindribbler/spring/Application.java
+++ src/main/java/com/braindribbler/spring/Application.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Starting BrainDribbler Spring Application...");
-		System.out.println("Java Version: " + System.getProperty("java.version"));
-		System.out.println("DB_USER: " + System.getProperty("spring.datasource.username"));
 		SpringApplication.run(Application.class, args);
 	}
 
