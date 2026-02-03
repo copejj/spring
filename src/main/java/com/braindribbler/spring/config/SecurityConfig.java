cab6b5150a47188365a639da0cfb0f1a0c27a086
Jeff Cope <jeff.cope@gmail.com>
2026-02-03 15:16:15 -0700

    adding styling and linking fixes

--- src/main/java/com/braindribbler/spring/config/SecurityConfig.java
+++ src/main/java/com/braindribbler/spring/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login", "/", "/home", "/about", "/css/**").permitAll() // Public paths
+                .requestMatchers("/", "/login", "/about", "/css/**", "/images/**", "/favicon.ico").permitAll() // Public paths
                 .anyRequest().authenticated() // Everything else is locked
             )
             .formLogin(form -> form
