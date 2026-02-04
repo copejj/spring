bdf91460add1731a413d9d89c3c94b42bfcb4c8c
Jeff Cope <jeff.cope@gmail.com>
2026-02-04 09:29:43 -0700

    Switching to a CompanyDTO

--- sql/inc/updates.sql
+++
@@ -1,8 +0,0 @@
--- push password hash updates for users 1 and 3, change immediately
-update users t1
-set password_hash = t2.password_hash
-from (
-	values (1, '$argon2id$v=19$m=16384,t=2,p=1$1/DzKh81rXYBR40+G9GlYw$6i5lgtB+OXgXpAOzGP677gb86lRvjKKj9QNzf4h73bo'),
-		(3, '$argon2id$v=19$m=16384,t=2,p=1$1/DzKh81rXYBR40+G9GlYw$6i5lgtB+OXgXpAOzGP677gb86lRvjKKj9QNzf4h73bo')
-) as t2(user_id, password_hash)
-where t1.user_id = t2.user_id;
--- src/main/java/com/braindribbler/spring/controllers/companies/CompanyController.java
+++ src/main/java/com/braindribbler/spring/controllers/companies/CompanyController.java
@@ -6,44 +6,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.braindribbler.spring.controllers.BaseController;
 import com.braindribbler.spring.repositories.companies.CompanyRepository;
 import com.braindribbler.spring.security.UserDetailsImpl;
 
 @Controller
+@RequestMapping("/companies")
 public class CompanyController extends BaseController {
 
 	@Autowired
 	private CompanyRepository companyRepository;
 	
 	/**
-	 * Looks up the companies for the given user ID.
-	 * @param userId The target user ID.
-	 * @param model
-	 * @return The companies list view.
-	 */
-    @GetMapping("/companies/{userId}/userid")
-    public String getCompaniesByUserId(@PathVariable Long userId, Model model) {
-		model.addAttribute("menus", getDefaultMenus("companies"));
-		model.addAttribute("location", "Companies");
-		model.addAttribute("pageTitle", "Companies List");
-
-        model.addAttribute("companies", companyRepository.findByUserId(userId));
-		model.addAttribute("view", "companies/list :: companyTable(companies=${companies})");
-        return "companies/list";
-    }
-
-	/**
 	 * Looks up the companies for the currently authenticated user.
 	 * @param userDetails The authenticated user details.
 	 * @param model
 	 * @return
 	 */
-	@GetMapping("/companies")
+	@GetMapping
 	public String getCompaniesByCurrentUser(@AuthenticationPrincipal UserDetailsImpl userDetails, Model model) {
-		return getCompaniesByUserId(userDetails.getUserId(), model);
-	}
+		if (userDetails == null) {
+			return "redirect:/login";
+		} 
+
+        model.addAttribute("companies", companyRepository.findByUserId(userDetails.getUserId()));
+
+		model.addAttribute("menus", getDefaultMenus("companies"));
+		model.addAttribute("location", "Companies");
+		model.addAttribute("pageTitle", "Companies List");
+
+		model.addAttribute("view", "companies/list :: companyTable(companies=${companies})");
+        return "companies/list";
+    }
 
 	/**
 	 * Display a single company by its ID.
@@ -51,13 +47,13 @@ public class CompanyController extends BaseController {
 	 * @param model
 	 * @return The company detail view.
 	 */
-	@GetMapping("/companies/{companyId}")
+	@GetMapping("/edit/{companyId}")
 	public String getCompanyById(@PathVariable Long companyId, Model model) {
 		model.addAttribute("menus", getDefaultMenus("companies"));
 		model.addAttribute("location", "Company Details");
 		model.addAttribute("pageTitle", "Company Information");	
 		
 		model.addAttribute("company", companyRepository.findById(companyId).orElse(null));
-		return "companies/detail";
+		return "companies/edit";
 	}
 }
