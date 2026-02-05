6185747c1192c3a1e389fc9125b3344da262f030
Jeff Cope <jeff.cope@gmail.com>
2026-02-05 10:47:38 -0700

    different attempt

--- sql/migrations/20260205_01_hash_update.sql
+++ sql/migrations/20260205_01_hash_update.sql
@@ -1,7 +1,7 @@
 update users t1
 set password_hash = t2.password_hash
 from (
-	values (1, '$argon2id$v=19$m=16384,t=2,p=1$RHxRPji8qmhIE2XSFCUBlA$y5weWAThhAxEMhZb8sVZSN+7ctFpAsOAV+ootvqZ18c'),
-		(3, '$argon2id$v=19$m=16384,t=2,p=1$ZgkfroTVXkrlOJJph28NOA$wEKMZ4X288fWIe9uCD1Lrahw6Vzi3p9hIvAQe5hI7LM')
+	values (1, '$argon2i$v=19$m=65536,t=4,p=1$TUhMTXhXSDZZR1J3c3JUUA$T+2vDfmO7TuJhG2yKpU9LGjn9VAX6YdYb1PmbtLe3JE'),
+		(3, '$argon2i$v=19$m=65536,t=4,p=1$TUhMTXhXSDZZR1J3c3JUUA$T+2vDfmO7TuJhG2yKpU9LGjn9VAX6YdYb1PmbtLe3JE')
 ) as t2(user_id, password_hash)
 where t1.user_id = t2.user_id;
