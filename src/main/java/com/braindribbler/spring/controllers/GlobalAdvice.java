559bed66de0089c9a9c71da909e2670c19d1fe1d
Jeff Cope <jeff.cope@gmail.com>
2026-02-10 04:59:39 -0700

    adding version changes

--- src/main/java/com/braindribbler/spring/controllers/GlobalAdvice.java
+++ src/main/java/com/braindribbler/spring/controllers/GlobalAdvice.java
@@ -2,7 +2,6 @@ package com.braindribbler.spring.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.GitProperties;
-import org.springframework.core.env.Environment;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,14 +9,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class GlobalAdvice {
 
-	@Autowired
-	private Environment environment;
-
 	@Autowired(required = false)
     private GitProperties gitProperties;
 
     @ModelAttribute
-    public void addGitInfo(Model model) {
+    public void addGitInfo2(Model model) {
         if (gitProperties != null) {
             // 1. Try to get the tag first (best for Production)
             String version = gitProperties.get("closest.tag.name");
@@ -28,6 +24,25 @@ public class GlobalAdvice {
             }
 
             model.addAttribute("gitBranch", version);
+            model.addAttribute("gitHash", gitProperties.getShortCommitId());
+        } else {
+            model.addAttribute("gitBranch", "unknown");
+            model.addAttribute("gitHash", "0000");
+        }
+    }
+
+    @ModelAttribute
+    public void addGitInfo(Model model) {
+        if (gitProperties != null) {
+            // 1. Get the tag (Populated when on a tag/detached HEAD in prod)
+            String tag = gitProperties.get("closest.tag.name");
+            // 2. Get the branch (Returns hash in detached HEAD, or "main" in dev)
+            String branch = gitProperties.getBranch();
+
+            // If we have a valid tag, use it; otherwise use the branch name
+            String displayVersion = (tag != null && !tag.isEmpty()) ? tag : branch;
+
+            model.addAttribute("gitBranch", displayVersion);
             model.addAttribute("gitHash", gitProperties.getShortCommitId());
         } else {
             model.addAttribute("gitBranch", "unknown");
